f42b6b4107464dcd2bf02099dfb354a2a72ff610
IwamotoKakeru <kganpipi@gmail.com>
2022-06-12 18:42:14 +0900

    講習会用にコードを削除

--- Assets/Script/Canon.cs
+++ Assets/Script/Canon.cs
@@ -14,9 +14,5 @@ public class Canon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
-           //bulletを自分と同じ位置に生成
-           Instantiate(bullet,this.transform.position,transform.rotation); 
-        }    
     }
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    private float speed = 0.1f;     //speedという小数の変数を用意して代入
-    private float rotSpeed = 0.3f;
 
     void Start()//始まったときに1回だけ実行される場所
     {
@@ -15,21 +13,6 @@ public class Player : MonoBehaviour
 
     void Update()//毎回実行される場所
     {
-        if(Input.GetKey(KeyCode.W)){ //Wが押されたとき
-            transform.Translate(0,0,speed); //毎回speedメートルずつ前に動く
-        }
-
-        if(Input.GetKey(KeyCode.S)){//Sが押されたとき
-            transform.Translate(0,0,-speed);
-        }
-
-        if(Input.GetKey(KeyCode.D)){
-            transform.Rotate(new Vector3(0,rotSpeed,0)); //毎回右にrotSpeed度ずつ回転する
-        }
-
-        if(Input.GetKey(KeyCode.A)){
-            transform.Rotate(new Vector3(0,-rotSpeed,0)); 
-        }
     }
 
 }
